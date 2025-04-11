bd0fc1fdd5d508272c9de173d08e3aac31cf283c
Suraj Hanvate <surajhanvate999@gmail.com>
2025-04-11 10:42:30 +0530

    Created Order and OrderItem entities

---
+++ src/main/java/com/suraj/ShoppingCart/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.suraj.ShoppingCart.enums;
+
+public enum OrderStatus {
+	PENDING,
+	PROCESSING,
+	SHIPPED,
+	DELIVERED,
+	CANCELLED
+}
---
+++ src/main/java/com/suraj/ShoppingCart/model/Order.java
@@ -0,0 +1,32 @@
+package com.suraj.ShoppingCart.model;
+
+import com.suraj.ShoppingCart.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long orderId;
+	private LocalDate orderDate;
+	private BigDecimal totalAmount;
+
+	@Enumerated(EnumType.STRING)
+	private OrderStatus orderStatus;
+
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+	private Set<OrderItem> orderItems;
+}
